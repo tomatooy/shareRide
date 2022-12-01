b70c30314a14f55d93351d9130ddc82c6c239062
ckgluck <ckgluck@gmail.com>
2022-11-30 20:03:30 -0500

    reworded Ride class

--- app/src/main/java/edu/uga/cs/shareride/Ride.java
+++ app/src/main/java/edu/uga/cs/shareride/Ride.java
@@ -1,5 +1,6 @@
 package edu.uga.cs.shareride;
 
+/**
 import java.util.Date;
 
 public class Ride {
@@ -52,5 +53,86 @@ public class Ride {
 
     public String getTo() {
         return To;
+    }
+}
+*/
+
+public class Ride {
+    private Boolean isOffer;
+    private Boolean isAccepted;
+    private Boolean isConfirmed;
+    private String driverID;
+    private Integer pointCost;
+    private String destLocation;
+    private String startLocation;
+    private String date;
+
+    public Ride() {
+        this.isOffer = null;
+        this.isAccepted = false;
+        this.isConfirmed = false;
+        this.driverID = null;
+        this.pointCost = null;
+        this.destLocation = null;
+        this.startLocation = null;
+        this.date = null;
+    }
+
+    public Ride(Boolean offer, Integer points, String destination, String start, String time) {
+        this.isOffer = offer;
+        this.pointCost = points;
+        this.destLocation = destination;
+        this.startLocation = start;
+        this.date = time;
+    }
+
+    // getters
+    public Boolean getIsOffer() {
+        return isOffer;
+    }
+    public Boolean getIsAccepted() {
+        return isAccepted;
+    }
+    public Boolean getIsConfirmed() {
+        return isConfirmed;
+    }
+    public String getDriverID() {
+        return driverID;
+    }
+    public Integer getPointCost() {
+        return pointCost;
+    }
+    public String getDestLocation() {
+        return destLocation;
+    }
+    public String getStartLocation() {
+        return startLocation;
+    }
+    public String getDate() { return date; }
+
+    // setters
+    public void setIsOffer(Boolean isOffer) {
+        this.isOffer = isOffer;
+    }
+    public void setIsAccepted(Boolean isAccepted) {
+        this.isAccepted = isAccepted;
+    }
+    public void setIsConfirmed(Boolean isConfirmed) {
+        this.isConfirmed = isConfirmed;
+    }
+    public void setDriverID(String driverID) {
+        this.driverID = driverID;
+    }
+    public void setPointCost(Integer pointCost) {
+        this.pointCost = pointCost;
+    }
+    public void setDestLocation(String destLocation) {
+        this.destLocation = destLocation;
+    }
+    public void setStartLocation(String pickupLocation) {
+        this.startLocation = pickupLocation;
+    }
+    public void setDate(String date) {
+        this.date = date;
     }
 }
